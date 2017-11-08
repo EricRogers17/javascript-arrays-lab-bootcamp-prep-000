2048b4e66954675b2666c2632318d8a12c65df9a
Eric Rogers <eric.rog17@gmail.com>
2017-11-08 23:17:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function destructivelyRemoveFirstKitten() {
 function appendKitten(name) {
   // preserve the underlying array by creating a new array.
   // add a new kitten to the new array and return it.
-  var newlyAppendedkittensArray = kittens[];
+  var newlyAppendedkittensArray = kittens;
   newlyAppendedkittensArray = newlyAppendedkittensArray.push(name);
   return newlyAppendedkittensArray;
 }
