c9d59dc65f90e758ab1a2e786e8f38ce8d037373
Eric Rogers <eric.rog17@gmail.com>
2017-11-08 23:04:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const app = "I don't do much."
 var kittens = ['Milo', 'Otis', 'Garfield'];
 
+function destructivelyAppendKitten(name) {
+  // effect the underlying array and return it
+  kittens.append(name);
+  return kittens;
+}
